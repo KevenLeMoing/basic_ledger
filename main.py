44bf341769f8fdef7ea3a3db23c53cc8ca79b308
lemoingk <KevenLeMoing@user-reglindiss-MacBook-Pro.local>
2021-12-09 17:44:00 +0100

    complete blockchain

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from src.sand_block import SandBlock
 
 def add_blocks(blockchain) -> SandChain:
     for i in range (1,5):
-        blockchain.get_new_block(blockchain.ledger[i-1])
+        blockchain.add_new_block(blockchain.ledger[i-1])
     return blockchain
 
-chain = SandChain(10)
+chain = SandChain()
 my_blockchain = add_blocks(chain)
 my_blockchain.print_chain_info()
-
-#block = SandBlock(block_id=0, prev_hash='', content='This is the first block of my first blockchain')
-#block.print_block_info()
--- src/sand_chain.py
+++ src/sand_chain.py
@@ -2,9 +2,8 @@ from src.sand_block import SandBlock
 
 
 class SandChain:
-    def __init__(self, ledger_length: int):
-        self.ledger = [self.set_first_block]
-        self.ledger_length = ledger_length
+    def __init__(self):
+        self.ledger = [self.set_first_block()]
 
     @staticmethod
     def set_first_block() -> SandBlock:
@@ -12,12 +11,12 @@ class SandChain:
                          prev_hash='',
                          content='This is the first block of my first blockchain')
 
-    def get_new_block(self, prev_block: SandBlock):
+    def add_new_block(self, prev_block: SandBlock):
         new_id = prev_block.block_id + 1
         prev_hash = prev_block.hash
         content = 'I am the block nbr {}'.format(new_id)
         self.ledger.append(SandBlock(new_id, prev_hash, content))
 
     def print_chain_info(self):
-        for block in len(self.ledger):
-            self.ledger[block].print_block_info()
+        for block in self.ledger:
+            block.print_block_info()
