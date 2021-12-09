1b8369c1d7b49fb13223b42554d3a94e48a020a5
lemoingk <KevenLeMoing@user-reglindiss-MacBook-Pro.local>
2021-12-09 11:46:32 +0100

    implement first block

--- sand_block.py
+++ sand_block.py
@@ -3,12 +3,19 @@ import hashlib
 
 
 class SandBlock:
-    def __init__(self, id: str, ts: datetime, prev_hash: str, content: str):
+    def __init__(self, id: int, ts: datetime, prev_hash: str, content: str):
         self.id = id
         self.ts = ts
         self.prev_hash = prev_hash
         self.content = content
         self.hash = self.hash_block()
+
+    @staticmethod
+    def get_first_block():
+        return SandBlock(id=0,
+                         ts=datetime.datetime.now(),
+                         prev_hash='',
+                         content='This is the first block of my first blockchain')
 
     @staticmethod
     def get_hashing_method():
@@ -22,3 +29,6 @@ class SandBlock:
         hashing_content = self.get_hashing_content()
         hashing_method.update(hashing_content)
         return hashing_method.hexdigest()
+
+    def get_new_block(self):
+        pass
